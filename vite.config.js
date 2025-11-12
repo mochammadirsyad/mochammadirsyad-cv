35d2e996dabec05f13bbd97072c1dc5fe30e88b2
Mochammad Irsyad <mochammadirsyad94@gmail.com>
2025-11-12 14:17:53 +0700

    feature: change url name to be "online-cv"

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/mochammadirsyad-cv/',
+  base: '/online-cv/',
   plugins: [react(), tailwindcss()],
 })
